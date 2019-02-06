5e6982d09991287504a9774697c035e361ac1385
hspaula <hudsonsistemasp@gmail.com>
2019-02-06 13:38:11 -0200

    Commit inicial do curso AlgaWorks SpringMVC

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,5 @@
+insert into categoria (nome) values ('Lazer');
+insert into categoria (nome) values ('Alimentação');
+insert into categoria (nome) values ('Supermercado');
+insert into categoria (nome) values ('Farmacia');
+insert into categoria (nome) values ('Outros');
